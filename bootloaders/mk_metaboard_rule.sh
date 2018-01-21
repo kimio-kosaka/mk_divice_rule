90a7144997e2b462cca16350040aca765b9226db
kimio_kosaka <kim.kosmac@gmail.com>
2018-01-21 09:27:41 +0900

    Update mk_metaboard_rule.sh

--- bootloaders/mk_metaboard_rule.sh
+++ bootloaders/mk_metaboard_rule.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 rule_file=/etc/udev/rules.d/60-objdev.rules
-str1=SUBSYSTEM=="usb", ATTR{product}=="USBasp", ATTR{idProduct}=="05dc", ATTRS{idVendor}=="16c0", MODE="0666"
+str1='SUBSYSTEM=="usb", ATTR{product}=="USBasp", ATTR{idProduct}=="05dc", ATTRS{idVendor}=="16c0", MODE="0666"'
 echo "Make roule file: $rule_file"
 if [ -e $rule_file ]; then
   echo "Already '$rule_file' is existing"
